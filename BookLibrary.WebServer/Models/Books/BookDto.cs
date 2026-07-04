1b07de1bf150719caa55fc2917a58c3235693879
Leonid Pasichnyi <leonid.on@outlook.com>
2026-07-04 15:55:44 +0300

    Patch

--- BookLibrary.WebServer/Models/Books/BookDto.cs
+++ BookLibrary.WebServer/Models/Books/BookDto.cs
@@ -1,24 +1,32 @@
 ﻿using BookLibrary.Storage.Models.Book;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace BookLibrary.WebServer.Models.Books
 {
     public class BookDto
     {
         public Guid? Id { get; set; }
+
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Name")]
         public string Name { get; set; }
+
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Authors")]
-        public string Authors { get; set; }
+        [BindProperty]
+        public List<string> Authors { get; set; } = [];
+
         [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Year")]
         public DateTime Year { get; set; }
+
         public bool? IsAvailable { get; set; }
 
         public BookDto()
@@ -30,7 +38,7 @@ namespace BookLibrary.WebServer.Models.Books
         {
             Id = book.Id;
             Name = book.Name;
-            Authors = string.Join(", ", book.Authors);
+            Authors = [.. book.Authors];
             Year = book.Year;
             IsAvailable = book.IsAvailable;
         }
@@ -39,11 +47,11 @@ namespace BookLibrary.WebServer.Models.Books
         {
             if (Id is null)
             {
-                return new Book(Name, Authors?.Split(", "), Year, IsAvailable ?? true);
+                return new Book(Name, Authors, Year, IsAvailable ?? true);
             }
             else
             {
-                return Book.FromPersistence((Guid)Id, Name, Authors?.Split(", "), Year, IsAvailable ?? true);
+                return Book.FromPersistence((Guid)Id, Name, Authors, Year, IsAvailable ?? true);
             }
         }
     }
