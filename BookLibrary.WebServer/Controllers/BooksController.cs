71abef7afa0bced9a4f305f07fe2e392f68af98e
Leonid Pasichnyi <leonid.on@outlook.com>
2026-07-04 19:39:45 +0300

    Patch

--- BookLibrary.WebServer/Controllers/BooksController.cs
+++ BookLibrary.WebServer/Controllers/BooksController.cs
@@ -18,7 +18,7 @@ namespace BookLibrary.WebServer.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBook(AddBookModel addBookModel)
+        public async Task<IActionResult> AddBook(BookDto addBookModel)
         {
             if (ModelState.IsValid)
             {
@@ -39,7 +39,7 @@ namespace BookLibrary.WebServer.Controllers
 
         public async Task<IActionResult> EditBook(Guid bookId)
         {
-            var book = new EditBookModel(await booksRepository.GetBook(bookId))
+            var book = new BookDto(await booksRepository.GetBook(bookId))
             {
                 Id = bookId
             };
@@ -48,7 +48,7 @@ namespace BookLibrary.WebServer.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> EditBook(EditBookModel editBookModel)
+        public async Task<IActionResult> EditBook(BookDto editBookModel)
         {
             var book = editBookModel.ToDomain();
             if (ModelState.IsValid)
